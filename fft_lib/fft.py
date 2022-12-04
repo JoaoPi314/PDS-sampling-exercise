59b43306550e154e4f42ee1394a06dee6b61f17c
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2022-12-04 12:11:02 -0300

    Implemented Wn calculation at FFT instantiation moment

--- fft_lib/fft.py
+++ fft_lib/fft.py
@@ -1,7 +1,21 @@
+import math
+import numpy as np
+
 class FFT():
     
     def __init__(self, N):
-        self.N = N
+        self.__set_N(N)
+        self.__set_Wn()
 
     def get_N(self):
         return self.N
+
+    def get_Wn(self):
+        return self.Wn
+
+    def __set_N(self, N):
+        self.N = 2**math.ceil(math.log2(N))
+
+    def __set_Wn(self):
+        W = np.exp(-1j*2*np.pi/self.N)
+        self.Wn = [W**i for i in range(0, self.N//2)]
